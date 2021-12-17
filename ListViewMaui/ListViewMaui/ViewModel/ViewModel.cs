c5927dbd775f651eb9c7c2408e03f0b01c085c5e
Rasika Palanisamy <rasika.palanisamy@gmail.com>
2021-12-17 15:50:59 +0530

    MAUI-832: Added custom sorting sample.

---
+++ ListViewMaui/ListViewMaui/ViewModel/ViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ListViewMaui
+{
+    public class ListViewSortingViewModel
+    {
+        #region Fields
+
+        private ObservableCollection<ListViewContactsInfo> contactsInfo;
+
+        #endregion
+
+        #region Constructor
+
+        public ListViewSortingViewModel()
+        {
+            GenerateSource(100);
+        }
+
+        #endregion
+
+        #region Properties
+
+        public ObservableCollection<ListViewContactsInfo> ContactsInfo
+        {
+            get { return contactsInfo; }
+            set { this.contactsInfo = value; }
+        }
+
+        #endregion
+
+        #region ItemSource
+
+        public void GenerateSource(int count)
+        {
+            ListViewContactsInfoRepository contactRepository = new ListViewContactsInfoRepository();
+            contactsInfo = contactRepository.GetContactDetails(count);
+        }
+
+        #endregion
+
+    }
+}
